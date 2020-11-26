c08090052eb2f68f6fd010abaa68cf5524faee55
Oleg Pozovnoy <pozovnoy.oleg@gmail.com>
2020-11-27 02:29:09 +0300

    final corrections

--- app.py
+++ app.py
@@ -9,10 +9,6 @@ import psycopg2
 import os
 
 
-# Telethon client
-#client = TelegramClient('SESSION', int(os.environ['TELEGRAM_API_ID']), os.environ['TELEGRAM_API_HASH'])
-#client.parse_mode = 'html'  # <- Render things nicely
-#client.flood_sleep_threshold = 60 
 quart_cfg = hypercorn.Config()
 port = int(os.environ.get("PORT", 17995))
 quart_cfg.bind = ["0.0.0.0:" + str(port)]
@@ -20,18 +16,6 @@ quart_cfg.bind = ["0.0.0.0:" + str(port)]
 app = Quart(__name__)
 app = cors(app, allow_origin="*")
 app.secret_key = 'CHANGE THIS TO SOMETHING SECRET'
-
-
-# Connect the client before we start serving with Quart
-#@app.before_serving
-#async def startup():
-#    await client.connect()
-
-
-# After we're done serving (near shutdown), clean up the client
-#@app.after_serving
-#async def cleanup():
-#    await client.disconnect()
 
 
 @app.route('/news', methods=['POST'], endpoint='news')
@@ -76,33 +60,22 @@ async def get_news_controller(content):
 
 
 async def get_news_model(channels, keywords):
-#    await client.start()
-#    print("GET NEWS MODEL") 
-#    data = {}
     errors = []
-
-    #for channel in channels:
-    #try: 
     msg = await get_channel(channels, keywords)
-    #        data[channel] = msg
-    #except Exception as e:
-    #        print(e)
-    #        errors.append(f"\nchannel {channel} import error.")
-#    await client.disconnect()
-    #print(msg)
     return msg, errors
 
 
 async def  get_channel(channels, keywords):
+    print("get_channels", channels, keywords)
     channel_querry = list(map(lambda x: "'" + str(x) + "'", channels))
     where_clause_channel = "channel in (" + ','.join(channel_querry) + ") and "
 
-    keywords_clause = list(map(lambda x: "or lower(message) like '%" + str(x) + "%'", keywords))
+    keywords_clause = list(map(lambda x: "or lower(message) like '%" + str(x).lower() + "%'", keywords))
     where_clause_keywords = '(False ' + ' '.join(keywords_clause) + ')'
 
-    query = 'select * from telegram where ' + where_clause_channel + where_clause_keywords + ' order by timestamp desc'
+    query = 'select * from telegram where ' + where_clause_channel + where_clause_keywords + ' order by timestamp desc LIMIT 30'
 
-    print(query)
+    #print(query)
     conn = psycopg2.connect( 
         user=user, 
         password=password,
@@ -122,26 +95,13 @@ async def  get_channel(channels, keywords):
     return my_list
 
 
-#    channel_id =await client.get_entity("https://t.me/" + channel)
-#    msgs = await client.get_messages(channel_id, limit = 1)
-
-#    result = []
-#    for msg in msgs:
-#        try:
-#            jsonify(msg.to_dict())
-#            result.append(msg.to_dict())
-#        except:
-#            pass
-#    return result
-
-
 @app.route('/quotes', methods=['POST'], endpoint='quotes')
 async def quotes_route():
     logging.info(request.is_json)
     content = await request.get_json()
-    print(content)
+    #print(content)
     response = await get_quotes_controller(content)
-    print(response)
+    #print(response)
     logging.info(f"response: {response}")
     return jsonify(response)
 
@@ -166,7 +126,7 @@ async def get_quotes_controller(content):
     else:
         response = {
             "state": "error",
-            "message": "unknown cpmmand",
+            "message": "unknown command",
         }
     return response 
 
@@ -174,33 +134,31 @@ async def get_quotes_controller(content):
 quotes_query_template = "\
     select t1.DATE, LOW, HIGH, OPEN, CLOSE, VOLUME  from\
     (\
-    select DATE, MIN(TIME) as OPEN_TIME, MAX(TIME) as CLOSE_TIME from quotes\
+    select timestamp::date as DATE, MIN(timestamp::time) as OPEN_TIME, MAX(timestamp::time) as CLOSE_TIME from allquotes\
     where TICKER ='%PLACEHOLDER%'\
     group by DATE) t1\
     inner join\
-    (select DATE, MIN(LOW) as LOW, MAX(HIGH) as HIGH, sum(VOL) as VOLUME from quotes\
+    (select timestamp::date as DATE, MIN(LOW) as LOW, MAX(HIGH) as HIGH, sum(VOL) as VOLUME from allquotes\
     where TICKER ='%PLACEHOLDER%'\
     group by DATE) t2\
     on t1.DATE = t2.DATE\
     inner join\
-    (select DATE, TIME, OPEN from quotes\
+    (select timestamp::date as DATE, timestamp::time as TIME, OPEN from allquotes\
     where TICKER ='%PLACEHOLDER%'\
     ) t3\
     on t1.OPEN_TIME = t3.TIME and t1.DATE = t3.DATE\
     inner join\
-    (select DATE, TIME, CLOSE from quotes\
+    (select timestamp::date as DATE, timestamp::time as TIME, CLOSE from allquotes\
     where TICKER ='%PLACEHOLDER%'\
     ) t4\
     on t1.CLOSE_TIME = t4.TIME and t1.DATE = t4.DATE\
     "
 
 
-
 user = os.environ['POSTGRES_USER']
 password = os.environ['POSTGRES_PASSWORD']
 host = os.environ['POSTGRES_HOST']
 db = os.environ['POSTGRES_DATABASE']
-
 
 
 def get_quotes_model(ticker):
@@ -225,4 +183,4 @@ def get_quotes_model(ticker):
     return my_list   
 
 
-app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 17995)))#, loop=client.loop) 
+app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 17995))) 
